18c78f9561d9e4a63912f0e582ea6ed8ec392b2f
sahulkola <mail2sahul.js@gmail.com>
2025-12-06 23:28:35 +0530

    feat: refactor application to integrated mono-repo with nx

--- apps/portfolio-angular/src/app/app.config.ts
+++ apps/portfolio-angular/src/app/app.config.ts
@@ -5,11 +5,12 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes), provideClientHydration(withEventReplay()),
   ],
 };
